db537a5de14ba74e0c0988f036b9ba8ca274761d
muhammad umair <m78075257@email.com>
2026-02-23 16:45:05 +0500

    done

--- src/Components/Shared/CenterNavbar.jsx
+++ src/Components/Shared/CenterNavbar.jsx
@@ -13,9 +13,9 @@ function CenterNavbar() {
     {  name: "Cart/ 411.76", icon: <FaShoppingCart /> }
   ];
   return (
-    <div className=" w-[96%] lg:justify-between flex items-center sm:justify-center  h-fit md:w-full justify-between  gap-0 sm:gap-[180px]"><div className="flex items-center  justify-between  w-[67%]    "><MenuNav/><Logo/><SearchBar/></div><div className="flex  m:gap-5 gap-[5px]">
+    <div className=" w-[98%] lg:justify-between flex items-center sm:justify-center  h-fit md:w-full justify-between  gap-0 sm:gap-[180px]"><MenuNav/><div className="flex items-center  justify-between  gap-5     "><Logo/><SearchBar/></div><div className="flex  m:gap-5 gap-[2px]">
   {products
-    .filter(item => item.id !== 2)
+    .filter(item => item.id !== 2 )
     .map(item => (
       <CenterNbtn
         key={item.id}
--- src/Components/Shared/CenterNbtn.jsx
+++ src/Components/Shared/CenterNbtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 function CenterNbtn({ name, icon ,className }) {
   return (
-    <button className={`border  cenbtn flex items-center sm:text-[18px] lg:w-fit lg:pt-2 lg:text-[13px] uppercase text-[12px] font-medium sm:py-2.5 py-2 rounded-md transition duration-300 gap-1 hover:bg-black hover:text-white px-2 ${className}`}>
+    <button className={`border  cenbtn flex items-center sm:text-[18px] lg:w-fit lg:pt-2 lg:text-[13px] uppercase text-[12px] font-medium sm:py-2.5 py-2 rounded-md transition duration-300 gap-1 hover:bg-black hover:text-white px-3 ${className}`}>
       <span className="hidden lg:inline">{name}</span>
-      <span>{icon}</span>
+      <span >{icon}</span>
     </button>
   )
 }
